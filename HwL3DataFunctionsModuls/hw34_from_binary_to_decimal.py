b914234504eab4b00a2f0dd3cdee1bb87c265aab
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-09-12 11:03:00 +0300

    Update hw34_from_binary_to_decimal.py

--- HwL3DataFunctionsModuls/hw34_from_binary_to_decimal.py
+++ HwL3DataFunctionsModuls/hw34_from_binary_to_decimal.py
@@ -5,22 +5,5 @@
 # - 3 -> 11
 # - 2 -> 10
 
-# Решение 1 - встроенная функция в Python
-# n = int(input())
-# print(bin(n))
-
-# Решение 2
-from msvcrt import kbhit
-
-
-n = int(input('Please input integer: ')) 
-k = ''
-while n > 0:
-    k = str(n % 2) + k
-    n = n // 2
-    print(n%2)
-    print(str(n % 2) + k)
-print(f'Binary number: {k}')
-
-
-
+n = int(input())
+print(bin(n))
