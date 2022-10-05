6e1d547055251dfc37a570fce891af37b14fd9e7
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-10-05 20:51:55 +0400

    Update hw61_unique_elements.py

--- HwL6AcceleratedDataProcessing/hw61_unique_elements.py
+++ HwL6AcceleratedDataProcessing/hw61_unique_elements.py
@@ -18,24 +18,18 @@ print(s)
 
 
 
-
-
-
-
-
-
 exit()
-from random import randint
-def create_list(size, m, n):
-    return [randint(m, n) for i in range(size)]
+# from random import randint
+# def create_list(size, m, n):
+#     return [randint(m, n) for i in range(size)]
 
-def get_unic_value(list):
-    return [i for i in set(list)]
+# def get_unic_value(list):
+#     return [i for i in set(list)]
 
-size = 10
-m = 1
-n = 10
+# size = 10
+# m = 1
+# n = 10
 
-origin = create_list(size, m, n)
-print(origin)
-print(get_unic_value(origin))
+# origin = create_list(size, m, n)
+# print(origin)
+# print(get_unic_value(origin))
